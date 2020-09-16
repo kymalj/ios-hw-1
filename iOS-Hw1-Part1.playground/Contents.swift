a62f9a1a4a8556917bcef840ace5ca54b270350a
kymalj <70738114+kymalj@users.noreply.github.com>
2020-09-15 19:46:27 -0700

    done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -5,21 +5,17 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
-
 
 var myWeight = 60
 var FriendWeight = 80.5
 var height = 1.5
 
+var weightSum = Double(myWeight) + FriendWeight
 
-var weightSum = myWeight + FriendWeight
-
-
-
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,47 +34,85 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "khalifa"
+var lastName1 = "aljraid"
+var email1 = "khalifa@gmail.com"
+var phoneNumber1 = 97426555
+var age1 = 16
+var country1 = "kuwait"
+var area1 = "fahaheel"
+var password = "1234"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var firstName2 = "aziz"
+var lastName2 = "alahmed"
+var email2 = "aziz@gmail.com"
+var phoneNumber2 = 97320386
+var age2 = 17
+var country2 = "kuwait"
+var area2 = "fahad alahmad"
+var password2 = "5739"
+var isKuwaiti2 = false
 
 //Person 3
-
-
-
-
-
-
-
+var firstName3 = "khaled"
+var lastName3 = "almajed"
+var email3 = "khaled@gmail.com"
+var phoneNumber3 = 95947263
+var age3 = 16
+var country3 = "kuwait"
+var area3 = "mangaf"
+var password3 = "1234"
+var isKuwaiti3 = true
 
 
 /*
 - - - -
 # المطلوب الثالث
 * قم بمقارنة معلومات كل شخص مع الشخص الاخر
- * قارن الاعمار اذا كانت متساوية ام لا
- * قارن منطقة السكن اذا كانت متساوية ام لا
- * قارن اذا عمر الاشخاص اكبر من ١٨
- * قارن اذا عمر الاشخاص اصغر من ١٨
- * قارن اذا عمر الاشخاص يساوي  ١٧
- * قارن اذا عمر الاشخاص لا يساوي  ١٧
 
+ * قارن الاعمار اذا كانت متساوية ام لا
+
+ * قارن منطقة السكن اذا كانت متساوية ام لا
+
+ * قارن اذا عمر الاشخاص اكبر من ١٨
  
+ * قارن اذا عمر الاشخاص اصغر من ١٨
+ 
+ * قارن اذا عمر الاشخاص يساوي  ١٧
+ 
+ * قارن اذا عمر الاشخاص لا يساوي  ١٧
  */
+age1 == age3 || age1 != age2 || age3 != age2
+area1 != area2 || area1 != area3 || area2 != area3
+age1 > 18
+age1 < 18
+age2 > 18
+age2 < 18
+age3 > 18
+age3 < 18
+age1 == 17
+age1 != 17
+age2 == 17
+age2 != 17
+age3 == 17
+age3 != 17
+
+print(" * * ")
+print("*   *  ")
+print(" * *     ******                   **")
+print("        *       *                *   *")
+print("  *    *         *              *     * ")
+print("  *    *         *              *")
+print("  *    *         *              *")
+print("  *    *         *               *")
+print("  *    *         *                 *")
+print("  *    *         *                  *")
+print("  *    *         *                    *")
+print("  *    *         *                     *")
+print("  *    *         *                      *")
+print("  *    *         *                      *")
+print("  *    *         *               *      *")
+print("  *     *       *                 *     *")
+print("  *       ******                    *  *")
